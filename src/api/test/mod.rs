1dc1254969e0099f8a4fdc0866ab820985c16c23
Hugh Kaznowski <phkaznowski@gmail.com>
2023-07-15 22:58:25 +0100

    Fix test after cargo fix

--- src/api/test/mod.rs
+++ src/api/test/mod.rs
@@ -1,24 +1,26 @@
+#[cfg(test)]
+mod test {
+    use crate::api;
+    use axum::http::StatusCode;
+    use axum_test::TestServer;
 
+    #[tokio::test]
+    async fn test_games_can_be_created() {
+        let api = api::new(vec![]);
+        let app = api.make_server().await;
 
+        let server = TestServer::new(app).unwrap();
 
+        let response = server.get("/game/new").await;
 
+        response.assert_status(StatusCode::OK);
 
-#[tokio::test]
-async fn test_games_can_be_created() {
-    let api = api::new(vec![]);
-    let app = api.make_server().await;
+        let bytes = response.bytes();
+        let body_str = std::str::from_utf8(&bytes).unwrap();
 
-    let server = TestServer::new(app).unwrap();
-
-    let response = server.get("/game/new").await;
-
-    response.assert_status(StatusCode::OK);
-
-    let bytes = response.bytes();
-    let body_str = std::str::from_utf8(&bytes).unwrap();
-
-    assert_eq!(
-        body_str,
-        r#"{"name":"new game name 0","dataset":"new game dataset 0"}"#
-    );
+        assert_eq!(
+            body_str,
+            r#"{"name":"new game name 0","dataset":"new game dataset 0"}"#
+        );
+    }
 }
