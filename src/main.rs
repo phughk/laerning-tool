5c19c23f77321067bd4d86adfa1556660c304131
Hugh Kaznowski <phkaznowski@gmail.com>
2023-07-15 19:18:42 +0100

    Working server + component diagram

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use hyper::Server;
+use std::net::SocketAddr;
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    // Create a new Axum router
+    let app = Router::new().route("/", get(hello_world));
+
+    // Define the address on which the server will listen
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // Start the server
+    println!("Server running on http://{}", addr);
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
