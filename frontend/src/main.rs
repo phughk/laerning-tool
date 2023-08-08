45932d29ce2f4130e08882364a37ced0a2f27938
Hugh Kaznowski <phkaznowski@gmail.com>
2023-08-08 21:42:16 +0100

    Add Dioxus

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,25 @@
+#![allow(non_snake_case)]
+
+// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
+use dioxus::prelude::rsx;
+use dioxus::prelude::Element;
+
+use dioxus::prelude::dioxus_elements;
+use dioxus::prelude::use_state;
+use dioxus::prelude::Scope;
+
 fn main() {
-    println!("Hello, world!");
+    // launch the web app
+    dioxus_web::launch(App);
+}
+
+// create a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
+    let mut count = use_state(cx, || 0);
+
+    cx.render(rsx!(
+        h1 { "High-Five counter: {count}" }
+        button { onclick: move |_| count += 1, "Up high!" }
+        button { onclick: move |_| count -= 1, "Down low!" }
+    ))
 }
