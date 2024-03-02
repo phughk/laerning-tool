fe40c5612b7de2337bd52f11930d56767d444189
Hugh Kaznowski <phkaznowski@gmail.com>
2024-03-02 17:23:21 +0000

    ratatui demo

--- cli-tui/src/main.rs
+++ cli-tui/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::event::{KeyCode, KeyEventKind};
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+use crossterm::{event, ExecutableCommand};
+use ratatui::backend::CrosstermBackend;
+use ratatui::style::Stylize;
+use ratatui::widgets::Paragraph;
+use ratatui::Terminal;
+use std::error::Error;
+use std::io::stdout;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
+
+    loop {
+        terminal.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(
+                Paragraph::new("Hello Ratatui! (press 'q' to quit)")
+                    .white()
+                    .on_blue(),
+                area,
+            );
+        })?;
+
+        if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
